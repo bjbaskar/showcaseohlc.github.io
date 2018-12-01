ffe3f0c39e2ea75325515e4e3c5a3f773452f7c3
Baskaran Balaganesan <bjbaskar@yahoo.com>
2018-12-01 11:30:02 +0800

    initial commit to release

---
+++ precache-manifest.eafb5aea9e239b2ce2827b95f916fa1f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6e5b86d74352699fff02e4bdc5185e5",
+    "url": "/static/media/muli-latin-600.b6e5b86d.woff2"
+  },
+  {
+    "revision": "9c28b10d1160dc1a93c9",
+    "url": "/static/css/main.d0b12402.chunk.css"
+  },
+  {
+    "revision": "f1189aead3bed133b2999d963aa4273f",
+    "url": "/static/media/muli-latin-900italic.f1189aea.woff"
+  },
+  {
+    "revision": "0419ae40152ab23caff2",
+    "url": "/static/js/1.0419ae40.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4387a458edb3bec4e81602c6329a207f",
+    "url": "/static/media/muli-latin-200.4387a458.woff2"
+  },
+  {
+    "revision": "eb2edd0dc666070fc7b90d79c890d8eb",
+    "url": "/static/media/muli-latin-200italic.eb2edd0d.woff2"
+  },
+  {
+    "revision": "3ae45760f8b0b342216bd55f131a68ec",
+    "url": "/static/media/muli-latin-200.3ae45760.woff"
+  },
+  {
+    "revision": "3d9d9afae68fc95977ec200c119c42a1",
+    "url": "/static/media/muli-latin-300.3d9d9afa.woff2"
+  },
+  {
+    "revision": "178072369c57c2d0a71adaea74e543f1",
+    "url": "/static/media/muli-latin-200italic.17807236.woff"
+  },
+  {
+    "revision": "812c1a52c772b9ac9b4f7acc93deaa39",
+    "url": "/static/media/muli-latin-300.812c1a52.woff"
+  },
+  {
+    "revision": "351cf102533282a857656415ded19e6e",
+    "url": "/static/media/muli-latin-300italic.351cf102.woff2"
+  },
+  {
+    "revision": "fe0f275136e0a53984c3bdb64edd6076",
+    "url": "/static/media/muli-latin-300italic.fe0f2751.woff"
+  },
+  {
+    "revision": "45e555f9d7a99c5b06bf70196c20abbe",
+    "url": "/static/media/muli-latin-400.45e555f9.woff2"
+  },
+  {
+    "revision": "e40161a93339af51847eee49c683f08f",
+    "url": "/static/media/muli-latin-400.e40161a9.woff"
+  },
+  {
+    "revision": "14dce6af9d49c83b9c67001b58fdeb0e",
+    "url": "/static/media/muli-latin-400italic.14dce6af.woff2"
+  },
+  {
+    "revision": "10a04c820381c42df50193e3163d4f9d",
+    "url": "/static/media/muli-latin-400italic.10a04c82.woff"
+  },
+  {
+    "revision": "9c28b10d1160dc1a93c9",
+    "url": "/static/js/main.9c28b10d.chunk.js"
+  },
+  {
+    "revision": "a2138124b73c9275bbdc71bfce6b2c0e",
+    "url": "/static/media/muli-latin-600.a2138124.woff"
+  },
+  {
+    "revision": "b2918fe9c48daf63ab74b130528bba96",
+    "url": "/static/media/muli-latin-600italic.b2918fe9.woff2"
+  },
+  {
+    "revision": "59eb241c89f0d85e4761fd202702d4a8",
+    "url": "/static/media/muli-latin-600italic.59eb241c.woff"
+  },
+  {
+    "revision": "8f65fa68cfb5d8cc4f4fa728a470332b",
+    "url": "/static/media/muli-latin-700.8f65fa68.woff2"
+  },
+  {
+    "revision": "1e6aab148ffeb8fe3470c15ef33c90bf",
+    "url": "/static/media/muli-latin-700.1e6aab14.woff"
+  },
+  {
+    "revision": "33dd03b37af876c59e39bc5d1e24ddf3",
+    "url": "/static/media/muli-latin-700italic.33dd03b3.woff2"
+  },
+  {
+    "revision": "280e317f3b13f6fb0b1499343cfb7f40",
+    "url": "/static/media/muli-latin-700italic.280e317f.woff"
+  },
+  {
+    "revision": "4ecc03e82b230e336e59b529decc537d",
+    "url": "/static/media/muli-latin-800.4ecc03e8.woff2"
+  },
+  {
+    "revision": "7019eda5ae48543436f9ddc39af4e648",
+    "url": "/static/media/muli-latin-800.7019eda5.woff"
+  },
+  {
+    "revision": "48826d71bc78c02f24e6aec813f3fb7f",
+    "url": "/static/media/muli-latin-800italic.48826d71.woff2"
+  },
+  {
+    "revision": "75f0022540a0a84f3bfeb37f1d8735e3",
+    "url": "/static/media/muli-latin-800italic.75f00225.woff"
+  },
+  {
+    "revision": "7f69621e9f4f5b5c7b6ea88b99cd2fcc",
+    "url": "/static/media/muli-latin-900.7f69621e.woff2"
+  },
+  {
+    "revision": "3f016a1fd0a668cfb421ddafb044ba30",
+    "url": "/static/media/muli-latin-900.3f016a1f.woff"
+  },
+  {
+    "revision": "83ecdc45fad2cb32a0897e4525a073e5",
+    "url": "/static/media/muli-latin-900italic.83ecdc45.woff2"
+  },
+  {
+    "revision": "0419ae40152ab23caff2",
+    "url": "/static/css/1.be4c5b0a.chunk.css"
+  },
+  {
+    "revision": "5a15412cb51b31151ffac21dc277156c",
+    "url": "/index.html"
+  }
+];
